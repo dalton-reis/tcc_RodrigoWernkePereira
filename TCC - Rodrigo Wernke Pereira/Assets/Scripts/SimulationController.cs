201c19144172d9c2af5e02d2d4a5efb65b6816ac
Rodrigo Wernke Pereira <rodrigow.pereira95@gmail.com>
2019-03-16 14:53:01 -0300

    First commit with project folder.
    Testing first commit.

---
+++ TCC - Rodrigo Wernke Pereira/Assets/Scripts/SimulationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SimulationController : MonoBehaviour
+{
+    private WindController _windController;
+
+    void Start()
+    {
+        _windController = new WindController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _windController.UpdateForces();
+    }
+}
---
+++ TCC - Rodrigo Wernke Pereira/Assets/Scripts/WindController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WindController : MonoBehaviour
+{
+    public void UpdateForces()
+    {
+        var trees = GameObject.FindGameObjectsWithTag("Tree");
+
+        foreach (var tree in trees)
+        {
+            // Adicionar controle com o marcador
+            
+            //folhas caindo
+            var particleSystemForceOverTimeModule = tree.GetComponentInChildren<ParticleSystem>().forceOverLifetime;
+            
+            particleSystemForceOverTimeModule.x = 1;
+            particleSystemForceOverTimeModule.z = 1;
+
+            //balanço dos galhos
+            var material = tree.GetComponentInChildren<MeshRenderer>().materials[1];
+
+            material.SetFloat("_tree_sway_speed", 1f);
+        }
+    }
+}
