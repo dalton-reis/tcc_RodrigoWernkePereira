c594e10902e074a2b7491f2a0286ab0deafd268c
Rodrigo Wernke Pereira <rodrigow.pereira95@gmail.com>
2019-03-17 20:12:23 -0300

    Wind Target
    Added wind target.

---
+++ TCC - Rodrigo Wernke Pereira/Assets/Scripts/Wind/WindTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WindTarget : MonoBehaviour
+{
+    private TextMesh _textMesh;
+
+    void Start()
+    {
+        _textMesh = GetComponentInChildren<TextMesh>();       
+    }
+
+    void Update()
+    {
+        var foward = transform.forward;
+
+        _textMesh.text = foward.x.ToString() + "x " + foward.y.ToString()
+            + "y " + foward.z.ToString() + "z";
+    }
+}
