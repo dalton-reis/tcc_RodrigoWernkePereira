d4c014207f5038d3c8e7b6032776ec462fffe92c
Rodrigo Wernke Pereira <rodrigow.pereira95@gmail.com>
2019-04-23 01:01:15 -0300

    Growth over time for trees!
    Added growth over time for trees, trying to simulate an animation. lol.

--- TCC - Rodrigo Wernke Pereira/Assets/Scripts/Trees/Tree.cs
+++ TCC - Rodrigo Wernke Pereira/Assets/Scripts/Trees/Tree.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
@@ -8,19 +9,31 @@ public class Tree : MonoBehaviour
     public TreeGrowthState TreeGrowthState;
 
     private Transform _treeTransform;
+
     private MeshRenderer _meshRenderer;
+
+    private ParticleSystem _leavesParticleSystem;
+
     private Material _dryLeafMaterial;
     private Material _windLeafMaterial;
+
+    private Color _dryLeafColor;
+    private Color _windLeafColor;
 
     void Start()
     {
         _treeTransform = GetComponent<Transform>();
         _meshRenderer = GetComponentInChildren<MeshRenderer>();
 
-        SetGrowthState();
+        _leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
 
         _dryLeafMaterial = Resources.Load("Materials/Tree/DryLeaf", typeof(Material)) as Material;
         _windLeafMaterial = Resources.Load("Materials/Tree/WindLeaves", typeof(Material)) as Material;
+
+        _dryLeafColor = new Color((185 / 255f), (122 / 255f), (87 / 255f));
+        _windLeafColor = _leavesParticleSystem.main.startColor.color;
+
+        SetGrowthState();
     }
 
     public void SetGrowthState()
@@ -29,15 +42,19 @@ public class Tree : MonoBehaviour
         {
             case TreeGrowthState.Seed:
                 {
-                    _treeTransform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    #region Scaling 
+                    var destinationScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    StartCoroutine(ScaleOverTime(1f, destinationScale));
+                    #endregion
 
-                    var leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
-
-                    if (leavesParticleSystem.isPlaying)
+                    #region Update Particle Leaves
+                    if (_leavesParticleSystem.isPlaying)
                     {
-                        leavesParticleSystem.Stop();
+                        _leavesParticleSystem.Stop();
                     }
+                    #endregion
 
+                    #region Update Material
                     var materials = _meshRenderer.materials;
 
                     if (materials[1].name.Equals("DryLeaf (Instance)"))
@@ -46,43 +63,65 @@ public class Tree : MonoBehaviour
 
                         _meshRenderer.materials = materials;
                     }
+                    #endregion
 
                     break;
                 }
             case TreeGrowthState.Sprout:
                 {
-                    _treeTransform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                    #region Scaling 
+                    var destinationScale = new Vector3(0.4f, 0.4f, 0.4f);
 
-                    var leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
+                    StartCoroutine(ScaleOverTime(1f, destinationScale));
+                    #endregion
 
-                    if (leavesParticleSystem.isPlaying)
+                    #region Update Particle Leaves
+                    if (_leavesParticleSystem.isPlaying)
                     {
-                        leavesParticleSystem.Stop();
+                        _leavesParticleSystem.Stop();
                     }
+                    #endregion
+
                     break;
                 }
             case TreeGrowthState.Sapling:
                 {
-                    _treeTransform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+                    #region Scaling 
+                    var destinationScale = new Vector3(0.6f, 0.6f, 0.6f);
+                    StartCoroutine(ScaleOverTime(1f, destinationScale));
+                    #endregion
 
+                    #region Update Particle Leaves
                     var leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
 
                     if (leavesParticleSystem.isPlaying)
                     {
                         leavesParticleSystem.Stop();
                     }
+                    #endregion
+
                     break;
                 }
             case TreeGrowthState.Mature:
                 {
-                    _treeTransform.localScale = new Vector3(1f, 1f, 1f);
+                    #region Scaling 
+                    var destinationScale = new Vector3(1f, 1f, 1f);
 
-                    var leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
+                    StartCoroutine(ScaleOverTime(1f, destinationScale));
+                    #endregion
 
-                    if (!leavesParticleSystem.isPlaying)
+                    #region Update Particle Leaves
+
+                    var mainModule = _leavesParticleSystem.main;
+                    mainModule.startColor = _windLeafColor;
+
+                    if (!_leavesParticleSystem.isPlaying)
                     {
-                        leavesParticleSystem.Play();
+                        _leavesParticleSystem.Play();
                     }
+
+                    #endregion
+
                     break;
                 }
             case TreeGrowthState.Snag:
@@ -95,26 +134,28 @@ public class Tree : MonoBehaviour
 
                     _meshRenderer.materials = materials;
                     #endregion
-                    #region Update Particle Leaves
-                    var leavesParticleSystem = GetComponentInChildren<ParticleSystem>();
 
-                    if (!leavesParticleSystem.isPlaying)
+                    #region Update Particle Leaves
+                    if (!_leavesParticleSystem.isPlaying)
                     {
-                        leavesParticleSystem.Play();
+                        _leavesParticleSystem.Play();
                     }
 
-                    var mainModule = leavesParticleSystem.main;
+                    var mainModule = _leavesParticleSystem.main;
 
-                    var dryLeafColor = new Color((185 / 255f), (122 / 255f), (87 / 255f));
-
-                    mainModule.startColor = dryLeafColor;
+                    mainModule.startColor = _dryLeafColor;
                     #endregion
+
+                    #region Scaling 
+                    var destinationScale = new Vector3(1.1f, 1.1f, 1.1f);
+                    StartCoroutine(ScaleOverTime(1f, destinationScale));
+                    #endregion
+
                     break;
                 }
             default:
                 return;
         }
-
     }
 
     public void UpdateGrowthState()
@@ -146,5 +187,19 @@ public class Tree : MonoBehaviour
                 TreeGrowthState = TreeGrowthState.Seed;
                 return;
         }
+    }
+
+    IEnumerator ScaleOverTime(float time, Vector3 destinationScale)
+    {
+        Vector3 originalScale = _treeTransform.localScale;
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            _treeTransform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
     }
 }
